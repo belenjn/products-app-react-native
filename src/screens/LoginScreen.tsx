123b7eb6abbd1d06e9afe862d015b6f53e9e12b8
Belen JN <belenjarabanavas31@gmail.com>
2022-12-27 15:56:13 +0100

    New theme and component created

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {} from 'react-native';
+import {Background} from '../components/Background';
 
 export const LoginScreen = () => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <>
+      <Background />
+    </>
   );
 };
