6a46b852de01ca8fca8cec7460b0a25c5536740c
Belen JN <belenjarabanavas31@gmail.com>
2022-12-28 11:35:33 +0100

    AuthContext and authReducer done

--- App.tsx
+++ App.tsx
@@ -2,11 +2,18 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {Navigator} from './src/navigator/Navigator';
 import {NavigationContainer} from '@react-navigation/native';
+import {AuthProvider} from './src/context/AuthContext';
+
+const AppState = ({children}: any) => {
+  return <AuthProvider>{children}</AuthProvider>;
+};
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Navigator />
+      <AppState>
+        <Navigator />
+      </AppState>
     </NavigationContainer>
   );
 };
