c0151ae1710623a89ffc44cd848b82063247d429
Belen JN <belenjarabanavas31@gmail.com>
2022-12-29 11:12:57 +0100

    Title in ProductScreen changed

--- src/screens/ProductScreen.tsx
+++ src/screens/ProductScreen.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useEffect} from 'react';
 import {Text, View} from 'react-native';
+import {StackScreenProps} from '@react-navigation/stack';
+import {ProductsStackParams} from '../navigator/ProductsNavigator';
 
-export const ProductScreen = () => {
+interface Props
+  extends StackScreenProps<ProductsStackParams, 'ProductsScreen'> {}
+
+export const ProductScreen = ({navigation, route}: Props) => {
+  const {id, name = ''} = route.params;
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: name ? name : 'Nuevo producto',
+    });
+  }, []);
+
   return (
     <View>
-      <Text>ProductScreen</Text>
+      <Text>{id}</Text>
+      <Text>{name}</Text>
     </View>
   );
 };
--- src/screens/ProductsScreen.tsx
+++ src/screens/ProductsScreen.tsx
@@ -1,19 +1,46 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react-native/no-inline-styles */
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import {FlatList, Text, View, StyleSheet} from 'react-native';
 import {ProductsContext} from '../context/ProductsContext';
 import {TouchableOpacity} from 'react-native-gesture-handler';
+import {StackScreenProps} from '@react-navigation/stack';
+import {ProductsStackParams} from '../navigator/ProductsNavigator';
 
-export const ProductsScreen = () => {
+interface Props
+  extends StackScreenProps<ProductsStackParams, 'ProductsScreen'> {}
+
+export const ProductsScreen = ({navigation}: Props) => {
   const {products, loadProducts} = useContext(ProductsContext);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          activeOpacity={0.8}
+          style={{marginRight: 10}}
+          onPress={() => navigation.navigate('ProductScreen', {})}>
+          <Text>Agregar</Text>
+        </TouchableOpacity>
+      ),
+    });
+  }, []);
+
   return (
     <View style={{flex: 1, marginHorizontal: 5}}>
       <FlatList
         data={products}
         keyExtractor={(p, index) => p._id + index}
         renderItem={({item}) => (
-          <TouchableOpacity activeOpacity={0.8}>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={() =>
+              navigation.navigate('ProductScreen', {
+                id: item._id,
+                name: item.nombre,
+              })
+            }>
             <Text style={styles.productName}>{item.nombre}</Text>
           </TouchableOpacity>
         )}
