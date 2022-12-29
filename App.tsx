e48305053cda0ce8f71014c12356b67f4663f508
Belen JN <belenjarabanavas31@gmail.com>
2022-12-29 10:16:10 +0100

    ProductsContext created

--- App.tsx
+++ App.tsx
@@ -3,9 +3,14 @@ import React from 'react';
 import {Navigator} from './src/navigator/Navigator';
 import {NavigationContainer} from '@react-navigation/native';
 import {AuthProvider} from './src/context/AuthContext';
+import {ProductsProvider} from './src/context/ProductsContext';
 
 const AppState = ({children}: any) => {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <AuthProvider>
+      <ProductsProvider>{children}</ProductsProvider>
+    </AuthProvider>
+  );
 };
 
 const App = () => {
